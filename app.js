18b30e22e24cf944351094c0ab5f5d37df83b68a
RACE (Rasmus Cederdorff) <rasmus@cederdorff.com>
2024-04-09 12:34:09 +0200

    Add contact selection and notification functionality

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// ========== Select Contacts ========== //
+
 if ("contacts" in navigator && "ContactsManager" in window) {
   getContacts();
 }
@@ -40,3 +42,44 @@ async function getContacts() {
     showContacts(contacts);
   });
 }
+
+// ========== Show Notification ========== //
+
+async function getNotificationPermission() {
+  const notification = document.querySelector("#notification");
+  const sendButton = document.querySelector("#send");
+  const registration = await navigator.serviceWorker.getRegistration();
+
+  async function sendNotification() {
+    if (Notification.permission === "granted") {
+      showNotification(notification.value);
+    } else {
+      if (Notification.permission !== "denied") {
+        const permission = await Notification.requestPermission();
+
+        if (permission === "granted") {
+          showNotification(notification.value);
+        }
+      }
+    }
+  }
+  sendNotification();
+
+  function showNotification(body) {
+    const title = "What PWA Can Do Today";
+
+    const payload = {
+      body
+    };
+
+    if ("showNotification" in registration) {
+      registration.showNotification(title, payload);
+    } else {
+      new Notification(title, payload);
+    }
+  }
+
+  sendButton.addEventListener("click", sendNotification);
+}
+
+getNotificationPermission();
