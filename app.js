35ee8dc15a86c23b342178f199010ab845273677
RACE (Rasmus Cederdorff) <rasmus@cederdorff.com>
2024-04-09 10:38:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const props = ["name", "email", "tel", "address", "icon"];
 const opts = { multiple: true };
 const supported = "contacts" in navigator && "ContactsManager" in window;
+console.log("Supported: ", supported);
+console.log("Contacts: ", navigator);
 
 async function getContacts() {
+  console.log("Getting contacts...");
   if (supported) {
+    console.log("Supported!");
     const contacts = await navigator.contacts.select(props, opts);
     console.log(contacts);
   }
@@ -11,3 +15,28 @@ async function getContacts() {
 
 const btn = document.querySelector("#btn-contacts");
 btn.addEventListener("click", getContacts);
+
+if ("IdleDetector" in window) {
+  const idleBtn = document.getElementById("idle");
+  idleBtn.addEventListener("click", event => runIdleDetection());
+
+  async function runIdleDetection() {
+    const state = await IdleDetector.requestPermission();
+    console.log(state);
+
+    const idleDetector = new IdleDetector();
+
+    idleDetector.addEventListener("change", () => {
+      const { userState, screenState } = idleDetector;
+      console.log(idleDetector);
+
+      if (userState == "idle") {
+        // update database with status
+      }
+    });
+
+    await idleDetector.start({
+      threshold: 120000
+    });
+  }
+}
