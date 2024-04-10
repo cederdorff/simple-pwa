c4ff1bd03dce6e6b459df14694afc0f885e9e38c
RACE (Rasmus Cederdorff) <rasmus@cederdorff.com>
2024-04-10 10:23:25 +0200

    Update service worker event listeners in service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
+// This code executes in its own worker or thread
 self.addEventListener("install", event => {
-  console.log("Service worker installing...");
-  // Place your files to cache here
+  console.log("Service worker installed");
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate", event => {
+  console.log("Service worker activated");
+});
+
+self.addEventListener(fetch, event => {
+  console.log("Inside the fetch handler:", event);
 });
